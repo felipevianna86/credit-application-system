6854ec93b8ecb38e8fbfbebb36a3fef835a20511
Felipe Vianna <felipe.vianna86@gmail.com>
2024-01-15 19:15:18 -0300

    Implementando métodos do CreditService.

--- src/main/kotlin/com/example/credit/application/system/repository/CreditRepository.kt
+++ src/main/kotlin/com/example/credit/application/system/repository/CreditRepository.kt
@@ -2,7 +2,15 @@ package com.example.credit.application.system.repository
 
 import com.example.credit.application.system.model.Credit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
-interface CreditRepository: JpaRepository<Credit, Long>
+interface CreditRepository: JpaRepository<Credit, Long>{
+
+    fun findByCreditCode(creditCode: UUID): Credit
+
+    @Query(value = "SELECT C FROM CREDIT C WHERE C.CUSTOMER_ID = ?1 ", nativeQuery = true)
+    fun fidnAllByCustomer(customerId: Long): List<Credit>
+}
---
+++ src/main/kotlin/com/example/credit/application/system/service/impl/CreditService.kt
@@ -0,0 +1,31 @@
+package com.example.credit.application.system.service.impl
+
+import com.example.credit.application.system.model.Credit
+import com.example.credit.application.system.repository.CreditRepository
+import com.example.credit.application.system.service.ICreditService
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class CreditService(
+        private val creditRepository: CreditRepository,
+        private val customerService: CustomerService
+): ICreditService {
+
+    override fun save(credit: Credit): Credit {
+        credit.apply {
+            customer = credit.customer?.id?.let { customerService.findById(it) }
+        }
+
+        return this.creditRepository.save(credit)
+    }
+
+    override fun findAllByCustomer(customerId: Long): List<Credit> =
+        this.creditRepository.fidnAllByCustomer(customerId)
+
+    override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
+        val creditDB: Credit = this.creditRepository.findByCreditCode(creditCode)
+
+        return if(creditDB.customer?.id == customerId) creditDB else throw RuntimeException("Contact admin.")
+    }
+}
