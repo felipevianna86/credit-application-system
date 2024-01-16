4ef7eb0d1e0869c2c1b8d4cfc7b887c895af4ac2
Felipe Vianna <felipe.vianna86@gmail.com>
2024-01-15 21:37:10 -0300

    Refatorações do projeto

--- src/main/kotlin/com/example/credit/application/system/controller/CreditResource.kt
+++ src/main/kotlin/com/example/credit/application/system/controller/CreditResource.kt
@@ -5,8 +5,10 @@ import com.example.credit.application.system.dto.CreditViewDTO
 import com.example.credit.application.system.dto.CreditViewListDTO
 import com.example.credit.application.system.model.Credit
 import com.example.credit.application.system.service.impl.CreditService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.util.UUID
+import java.util.*
 import java.util.stream.Collectors
 
 @RestController
@@ -16,26 +18,27 @@ class CreditResource(
 ) {
 
     @PostMapping
-    fun save(@RequestBody creditDTO: CreditDTO): String{
+    fun save(@RequestBody creditDTO: CreditDTO): ResponseEntity<String>{
         val creditSaved = this.creditService.save(creditDTO.toEntity())
-        return "Credit ${creditSaved.creditCode} - Customer ${creditSaved.customer?.firstName} was saved successfully!"
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Credit ${creditSaved.creditCode} - Customer ${creditSaved.customer?.firstName} was saved successfully!")
     }
 
     @GetMapping
-    fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long) : List<CreditViewListDTO> {
-        return this.creditService.findAllByCustomer(customerId).
+    fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long) : ResponseEntity<List<CreditViewListDTO>> {
+        return ResponseEntity.ok(this.creditService.findAllByCustomer(customerId).
                 stream().map {
                     credit: Credit -> CreditViewListDTO(credit)
-        }.collect(Collectors.toList())
+        }.collect(Collectors.toList()))
     }
 
     @GetMapping("/{creditCode}")
     fun findByCreditCode(
             @RequestParam(value = "customerId") customerId: Long,
             @PathVariable creditCode: UUID
-    ) : CreditViewDTO {
+    ) : ResponseEntity<CreditViewDTO> {
 
         val creditDB = this.creditService.findByCreditCode(customerId, creditCode)
-        return CreditViewDTO(creditDB)
+        return ResponseEntity.ok(CreditViewDTO(creditDB))
     }
 }
--- src/main/kotlin/com/example/credit/application/system/controller/CustomerResource.kt
+++ src/main/kotlin/com/example/credit/application/system/controller/CustomerResource.kt
@@ -4,15 +4,9 @@ import com.example.credit.application.system.dto.CustomerDTO
 import com.example.credit.application.system.dto.CustomerUpdateDTO
 import com.example.credit.application.system.dto.CustomerViewDTO
 import com.example.credit.application.system.service.impl.CustomerService
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/customer")
@@ -21,16 +15,16 @@ class CustomerResource(
 ) {
 
     @PostMapping
-    fun save(@RequestBody customerDTO: CustomerDTO) : String{
+    fun save(@RequestBody customerDTO: CustomerDTO) : ResponseEntity<String> {
         val customerSaved = this.customerService.save(customerDTO.toEntity())
-        return "Customer ${customerSaved.email} was saved"
+        return ResponseEntity.status(HttpStatus.CREATED).body("Customer ${customerSaved.email} was saved")
     }
 
     @GetMapping("/{customerId}")
-    fun findById(@PathVariable customerId: Long): CustomerViewDTO {
+    fun findById(@PathVariable customerId: Long): ResponseEntity<CustomerViewDTO> {
 
         val customerDB = this.customerService.findById(customerId);
-        return CustomerViewDTO(customerDB)
+        return ResponseEntity.ok(CustomerViewDTO(customerDB))
     }
 
     @DeleteMapping("/{customerId}")
@@ -38,10 +32,10 @@ class CustomerResource(
 
     @PatchMapping
     fun update(@RequestParam(value = "customerId") customerId: Long,
-               customerUpdateDTO: CustomerUpdateDTO): CustomerViewDTO {
+               customerUpdateDTO: CustomerUpdateDTO): ResponseEntity<CustomerViewDTO> {
         val customerDB = this.customerService.findById(customerId)
         val customerUpdated = this.customerService.save(customerUpdateDTO.toEntity(customerDB))
 
-        return CustomerViewDTO(customerUpdated)
+        return ResponseEntity.ok(CustomerViewDTO(customerUpdated))
     }
 }
