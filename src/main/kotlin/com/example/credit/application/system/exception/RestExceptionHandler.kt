e67578afe2f8cdb2774bed1b1778d333d0ec89e7
Felipe Vianna <felipe.vianna86@gmail.com>
2024-01-15 23:01:27 -0300

    Adicionando Handler para DataAccessException

--- src/main/kotlin/com/example/credit/application/system/exception/RestExceptionHandler.kt
+++ src/main/kotlin/com/example/credit/application/system/exception/RestExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.example.credit.application.system.exception
 
+import org.springframework.dao.DataAccessException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
@@ -24,14 +25,28 @@ class RestExceptionHandler {
             errors[fieldName] = messageError
         }
 
-        return ResponseEntity(
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                 DetailsException(
                         title = "Bad Request! Consult the documentation.",
                         timestamp = LocalDateTime.now(),
                         status = HttpStatus.BAD_REQUEST.value(),
                         exception = ex.javaClass.toString(),
                         details = errors
-                ), HttpStatus.BAD_REQUEST
+                )
         )
+    }
+
+    @ExceptionHandler(DataAccessException::class)
+    fun handlerValidException(ex: DataAccessException): ResponseEntity<DetailsException>{
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                DetailsException(
+                        title = "Conflict! Consult the documentation.",
+                        timestamp = LocalDateTime.now(),
+                        status = HttpStatus.BAD_REQUEST.value(),
+                        exception = ex.javaClass.toString(),
+                        details = mutableMapOf(ex.cause.toString() to ex.message)
+                )
+            )
     }
 }
